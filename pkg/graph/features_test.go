ca5e46220b3dacf3ad8a1eb0a1ebfbb7fd74c5ef
KissKim <130985882+KissKim@users.noreply.github.com>
2022-03-17 21:34:58 +0000

    Version 1.7.1

--- pkg/graph/features_test.go
+++ pkg/graph/features_test.go
@@ -14,4 +14,4 @@ func TestFeaturesClassWeights(t *testing.T) {
 		minority int
 		weights  []float64
 	}{
-		{43400, 5, 21, 783, []float64{0.8159428463996992, 0.810230561
+		{43400, 5, 21, 783, []float64{0.8159428463996992, 0.8102305610006534, 0.82345128545678
