cedb3c534269745339cd379e2137b6484b94a949
KissKim <130985882+KissKim@users.noreply.github.com>
2022-04-14 17:25:47 +0000

    release 0.0.1

--- pkg/graph/features_test.go
+++ pkg/graph/features_test.go
@@ -17,4 +17,5 @@ func TestFeaturesClassWeights(t *testing.T) {
 		{43400, 5, 21, 783, []float64{0.8159428463996992, 0.8102305610006534, 0.8234512854567878, 0.8093240093240093, 11.085568326947637}},
 	} {
 		t.Run(strconv.Itoa(k), func(t *testing.T) {
-			feature
+			features := Features{
+				X: m
