a3308154fc7c1f41e091efd39f88e35c2d4aefe4
KissKim <130985882+KissKim@users.noreply.github.com>
2022-05-14 12:25:57 +0000

    Update Setup.md

--- pkg/graph/features_test.go
+++ pkg/graph/features_test.go
@@ -24,4 +24,5 @@ func TestFeaturesClassWeights(t *testing.T) {
 			rand.Seed(80)
 			n := v.classes - 1
 			for i := 0; i < v.samples; i++ {
-				features.X[i] = make([]float64, v.fe
+				features.X[i] = make([]float64, v.features)
+				features.Y[i] = make
