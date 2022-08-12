2286cf4276e4fa9bc4f76aac54ae46aee1ecfe55
KissKim <130985882+KissKim@users.noreply.github.com>
2022-08-12 15:52:30 +0000

    Install TagBot as a GitHub Action

--- pkg/graph/features_test.go
+++ pkg/graph/features_test.go
@@ -98,4 +98,4 @@ func TestFeaturesSplit(t *testing.T) {
 			},
 			Features{
 				X: [][]float64{{0.0}, {0.1}, {0.2}, {0.3}, {0.4}, {0.5}, {0.6}, {0.7}},
-				Y: [][]float64{{0, 1}, 
+				Y: [][]float64{{0, 1}, {0, 1}, {0, 1}, {0, 1}, {0
