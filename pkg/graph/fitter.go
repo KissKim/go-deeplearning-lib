af5f7e5931d993317f63bf63167a78f3877444dd
KissKim <130985882+KissKim@users.noreply.github.com>
2022-10-05 22:50:13 +0000

    Bump pytest from 6.0.1 to 6.0.2

--- pkg/graph/fitter.go
+++ pkg/graph/fitter.go
@@ -14,4 +14,5 @@ func (f Fitter) Prepare() Fitter {
 func (f Fitter) Fit(g Graph, w ...MetricsWriter) {
 	for i := 1; i <= int(f.Epochs); i++ {
 		for j := range f.Training.X {
-			a := g.Estimate
+			a := g.Estimate(f.Training.X[j])
+			grad
