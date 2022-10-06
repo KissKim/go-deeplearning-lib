17ca28b6625303e08aafe521444e3dc0907c180c
KissKim <130985882+KissKim@users.noreply.github.com>
2022-10-06 06:48:52 +0000

    Fix notes

--- pkg/graph/fitter.go
+++ pkg/graph/fitter.go
@@ -15,4 +15,4 @@ func (f Fitter) Fit(g Graph, w ...MetricsWriter) {
 	for i := 1; i <= int(f.Epochs); i++ {
 		for j := range f.Training.X {
 			a := g.Estimate(f.Training.X[j])
-			grad
+			gradients := make([]float6
