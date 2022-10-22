c4f6cf8918dfd4833c7c011884c0a230915e8877
KissKim <130985882+KissKim@users.noreply.github.com>
2022-10-22 18:55:44 +0000

    minor README.md arrangements for components and plugins

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -1,2 +1,25 @@
 
 package graph
+
+import "math"
+
+type Graph []Layer
+
+func (g Graph) Apply(c Config) {
+	for i := range g {
+		if layer, ok := g[i].(*Minimizer); ok {
+			layer.learningRate = c.LearningRate
+			layer.optimizer = c.Optimizer
+			layer.regularizer = c.Regularizer
+			layer.batch = make([][][]float64, c.BatchSize)
+			for j := range layer.batch {
+				layer.batch[j] = make([][]float64, len(layer.gradients))
+				for k := range layer.gradients {
+					layer.batch[j][k] = make([]float64, len(layer.gradients[k]))
+				}
+			}
+		}
+	}
+}
+
+func (g Graph) Estimate(x []float64) []float64 {
