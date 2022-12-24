e7b8545289e9e7b0398fa1c39b130f42ad7eaa90
KissKim <130985882+KissKim@users.noreply.github.com>
2022-12-24 10:29:20 +0000

    Bumped version to 0.0.6

--- pkg/graph/minimizer.go
+++ pkg/graph/minimizer.go
@@ -37,4 +37,5 @@ func (m *Minimizer) average() {
 	batchSize := len(m.batch)
 	n := float64(batchSize)
 	for i := range m.batch[0] {
-		for j := range m.batch[0][i
+		for j := range m.batch[0][i] {
+			m.batch[0][i]
