669d88f5fce14143e039b5142cb4faf47249fc0e
KissKim <130985882+KissKim@users.noreply.github.com>
2023-01-16 19:58:59 +0000

    local change

--- pkg/layers/bias.go
+++ pkg/layers/bias.go
@@ -7,4 +7,6 @@ type Bias struct {
 
 func (b *Bias) Estimate(input []float64) []float64 {
 	for i := range input {
-		b.output[i] = input[i] + b.wei
+		b.output[i] = input[i] + b.weights[0][i]
+	}
+	return b.o
