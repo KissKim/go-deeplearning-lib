368e0d84f4819ad6bf46c96523640134c3257440
KissKim <130985882+KissKim@users.noreply.github.com>
2023-03-23 14:37:23 +0000

    Add stubs

--- pkg/layers/gradient_test.go
+++ pkg/layers/gradient_test.go
@@ -10,3 +10,21 @@ import (
 
 	"github.com/therfoo/therfoo/pkg/graph"
 )
+
+func TestGradient(t *testing.T) {
+	var linearGraph = func() graph.Graph {
+		return graph.New(
+			&Input{2},
+			&Dense{Neurons: 2},
+			&Sigmoid{},
+			&Dense{Neurons: 1},
+			&Sigmoid{},
+		)
+	}
+
+	var polynomialGraph = func() graph.Graph {
+		return graph.New(
+			&Input{2},
+			&Dense{Neurons: 1},
+			&Polynomial{Degree: 2},
+			&Sigmoid{},
