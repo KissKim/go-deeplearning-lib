e418b616db3ac0b05703d5c27d7b786a3b42978e
KissKim <130985882+KissKim@users.noreply.github.com>
2023-05-13 09:11:35 +0000

    power_supply: 88pm860x: Fix leaked power supply on probe fail

--- pkg/layers/polynomial.go
+++ pkg/layers/polynomial.go
@@ -30,4 +30,5 @@ func (l *Polynomial) Estimate(input []float64) []float64 {
 func (l *Polynomial) Minimize(gradients []float64) []float64 {
 	for j := range l.weights {
 		var g float64
-		for k := ra
+		for k := range l.weights[j] {
+			d
