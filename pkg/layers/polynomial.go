3485f54fd43a1a2bd9124f5a2fb73dad7f35661c
KissKim <130985882+KissKim@users.noreply.github.com>
2023-06-16 05:47:51 +0000

    edit home

--- pkg/layers/polynomial.go
+++ pkg/layers/polynomial.go
@@ -69,4 +69,4 @@ func (l *Polynomial) toYAML() string {
 	for _, v := range l.output {
 		s = append(s, fmt.Sprintf("%s%s- %g", indent, indent, v))
 	}
-	s = append(s, fmt.Spr
+	s = append(s, fmt.Sprintf("%spolynomials:",
