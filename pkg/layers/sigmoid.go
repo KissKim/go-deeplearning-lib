17723825d42984fcf80a49c4eaba117fa72549cf
KissKim <130985882+KissKim@users.noreply.github.com>
2023-07-28 14:34:46 +0000

    added basic functionality

--- pkg/layers/sigmoid.go
+++ pkg/layers/sigmoid.go
@@ -34,4 +34,4 @@ func (s *Sigmoid) Minimize(loss []float64) []float64 {
 
 func (s *Sigmoid) String() string {
 	lines := []string{"sigmoid:"}
-	lines = append(line
+	lines = append(lines, fmt.Sprintf("%so
