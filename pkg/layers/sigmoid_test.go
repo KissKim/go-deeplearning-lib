59238d7771faed48273626494385df3961225289
KissKim <130985882+KissKim@users.noreply.github.com>
2023-08-10 14:36:52 +0000

    Updating Dockerfiles This is an automated CI commit

--- pkg/layers/sigmoid_test.go
+++ pkg/layers/sigmoid_test.go
@@ -10,4 +10,4 @@ func TestSigmoidActivate(t *testing.T) {
 	var sigmoid = func(x float64) float64 {
 		return math.Exp(x) / (1.0 + math.Exp(x))
 	}
-	for k, v := r
+	for k, v := range []float64{-1000, 
