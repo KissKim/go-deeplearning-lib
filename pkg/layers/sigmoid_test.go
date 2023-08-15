3e9c6be463da974bcd48a9e2fe73d3bd2fae01e7
KissKim <130985882+KissKim@users.noreply.github.com>
2023-08-15 21:12:06 +0000

    Pontoon: Update Slovak (sk) localization of AMO Frontend

--- pkg/layers/sigmoid_test.go
+++ pkg/layers/sigmoid_test.go
@@ -11,4 +11,5 @@ func TestSigmoidActivate(t *testing.T) {
 		return math.Exp(x) / (1.0 + math.Exp(x))
 	}
 	for k, v := range []float64{-1000, -100, -10, 0, 10, 100, 1000} {
-		t.Run(strconv.Itoa(k), func(t *testing
+		t.Run(strconv.Itoa(k), func(t *testing.T) {
+			s := new(Sigmoid)
