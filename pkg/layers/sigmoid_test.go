6eb80603d99923e5a6112df3d3345af13e1c6344
KissKim <130985882+KissKim@users.noreply.github.com>
2023-08-25 15:25:49 +0000

    'add'

--- pkg/layers/sigmoid_test.go
+++ pkg/layers/sigmoid_test.go
@@ -31,4 +31,4 @@ func TestSigmoidDerive(t *testing.T) {
 		return math.Exp(-z) / (math.Pow(1+math.Exp(-z), 2))
 	}
 
-	for k, v := range []float64{0.1, 
+	for k, v := range []float64{0.1, 0.2, 0.3, 0.4, 0.5, 0.
