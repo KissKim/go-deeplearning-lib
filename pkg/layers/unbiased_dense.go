ae6a6e657f70786d13acf50fe15903c0899f92e4
KissKim <130985882+KissKim@users.noreply.github.com>
2023-09-18 22:27:22 +0000

    Ouch

--- pkg/layers/unbiased_dense.go
+++ pkg/layers/unbiased_dense.go
@@ -14,4 +14,5 @@ type UnbiasedDense struct {
 	Neurons   uint64
 }
 
-func (d *UnbiasedDense) Estimate(input []float64) []f
+func (d *UnbiasedDense) Estimate(input []float64) []float64 {
+	d.input = inpu
