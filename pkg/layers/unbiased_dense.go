c9a20260fc7625a044134693af956ec5f8918fb4
KissKim <130985882+KissKim@users.noreply.github.com>
2023-10-21 10:41:50 +0000

    fixed gcc warnings

--- pkg/layers/unbiased_dense.go
+++ pkg/layers/unbiased_dense.go
@@ -50,4 +50,5 @@ func (d *UnbiasedDense) SetShape(shape []uint64) {
 	d.localGradients = make([][]float64, n)
 	d.weights = make([][]float64, n)
 	for j := range d.weights {
-		d.localGradients[j] = 
+		d.localGradients[j] = make([]float64, w)
+		d.wei
