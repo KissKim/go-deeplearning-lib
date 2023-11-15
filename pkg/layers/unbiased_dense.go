1268181ef5026a29f5766c0b55a8c11fd9981d39
KissKim <130985882+KissKim@users.noreply.github.com>
2023-11-15 08:36:45 +0000

    Fix compilation warnings.

--- pkg/layers/unbiased_dense.go
+++ pkg/layers/unbiased_dense.go
@@ -62,4 +62,4 @@ func (d *UnbiasedDense) String() string {
 	for _, v := range d.input {
 		s = append(s, fmt.Sprintf("%s%s- %g", indent, indent, v))
 	}
-	s = append(
+	s = append(s, fmt.Sprintf("%soutputs:",
