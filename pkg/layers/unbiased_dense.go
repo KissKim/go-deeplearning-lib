78683ab3bc0b5524752f1a3700db5746228386ae
KissKim <130985882+KissKim@users.noreply.github.com>
2023-12-01 09:03:29 +0000

    deleted unused files

--- pkg/layers/unbiased_dense.go
+++ pkg/layers/unbiased_dense.go
@@ -69,4 +69,4 @@ func (d *UnbiasedDense) String() string {
 	s = append(s, fmt.Sprintf("%sweights:", indent))
 	for j := range d.weights {
 		s = append(s, fmt.Sprintf("%s%s-", indent, indent))
-		for k := r
+		for k := range d.weights[j]
