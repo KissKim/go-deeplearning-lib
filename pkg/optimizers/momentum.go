59c8448fe4e4d6daf40248148c6157e4f9d74753
KissKim <130985882+KissKim@users.noreply.github.com>
2024-02-10 16:05:15 +0000

    notebooks

--- pkg/optimizers/momentum.go
+++ pkg/optimizers/momentum.go
@@ -18,4 +18,5 @@ func (m Momentum) Apply(weights [][]float64) Momentum {
 
 func (m Momentum) Optimize(gradients [][]float64) [][]float64 {
 	for i := range gradients {
-		for j := range gradien
+		for j := range gradients[i] {
+			m.gradients[i][j] =
