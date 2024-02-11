7ebb540244c5d91d96dc817bcaa26fc40792df88
KissKim <130985882+KissKim@users.noreply.github.com>
2024-02-11 18:46:42 +0000

    added package-lock.json

--- pkg/optimizers/rmsprop.go
+++ pkg/optimizers/rmsprop.go
@@ -2,3 +2,26 @@
 package optimizers
 
 import "math"
+
+type RMSprop struct {
+	gradients [][]float64
+	momentum  float64
+}
+
+func (m RMSprop) Apply(weights [][]float64) RMSprop {
+	gradients := make([][]float64, len(weights))
+	for i := range gradients {
+		gradients[i] = make([]float64, len(weights[i]))
+	}
+	return RMSprop{
+		gradients: gradients,
+		momentum:  m.momentum,
+	}
+}
+
+func (m RMSprop) Optimize(gradients [][]float64) [][]float64 {
+	for i := range gradients {
+		for j := range gradients[i] {
+			m.gradients[i][j] = m.momentum*m.gradients[i][j] + (1-m.momentum)*math.Pow(gradients[i][j], 2)
+		}
+	}
